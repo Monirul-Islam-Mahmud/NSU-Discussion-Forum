dc59e34a8adc69659ea56253b01fb7f872802a17
Monirul Islam <mahmudislam2025@gmail.com>
2022-12-13 21:03:57 +0600

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php   
-    session_start(); //to ensure we are using same session
-    session_destroy(); //destroying the session
-    header("location:index.php"); //to redirect back to "index.php" after logging out
-    exit();
-?>
-
